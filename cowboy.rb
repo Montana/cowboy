b53f54c23720ec334f84b344e127cbfea78d424f
Montana Mendy <montanamendy@gmail.com>
2017-01-26 16:04:14 -0800

    Update cowboy.rb

--- cowboy.rb
+++ cowboy.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'capybara'
+require_relative 'test_helper'
 
 get('/') { 'this is cowboy yeehaw!' }
 
@@ -27,5 +29,63 @@ get '/stream', :provides => 'text/event-stream' do
   stream :keep_open do |out|
     connections << out
     out.callback { connections.delete(out) }
+  end
+end
+
+module Cowboy
+  class App < Sinatra::Base
+    set :environment, :test
+    set :reload_templates, true
+  end
+
+  module IntegrationTest
+    include Capybara::DSL
+
+    include ModelFactory
+    include TestConfiguration
+
+    def setup
+      Capybara.app = App.new
+    end
+
+    def teardown
+      Capybara.reset_sessions!
+      Capybara.use_default_driver
+
+      remove_temp_directory
+      Nesta::FileModel.purge_cache
+    end
+
+    def assert_has_xpath(query, options = {})
+      if ! page.has_xpath?(query, options)
+        message = "not found in page: '#{query}'"
+        message << ", #{options.inspect}" unless options.empty?
+        fail message
+      end
+    end
+
+    def assert_has_no_xpath(query, options = {})
+      if page.has_xpath?(query, options)
+        message = "found in page: '#{query}'"
+        message << ", #{options.inspect}" unless options.empty?
+        fail message
+      end
+    end
+
+    def assert_has_css(query, options = {})
+      if ! page.has_css?(query, options)
+        message = "not found in page: '#{query}'"
+        message << ", #{options.inspect}" unless options.empty?
+        fail message
+      end
+    end
+
+    def assert_has_no_css(query, options = {})
+      if ! page.has_no_css?(query, options)
+        message = "found in page: '#{query}'"
+        message << ", #{options.inspect}" unless options.empty?
+        fail message
+      end
+    end
   end
 end
